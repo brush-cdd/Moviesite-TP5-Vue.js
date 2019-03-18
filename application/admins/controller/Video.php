f1ee321bb67cdc2bd43a8dfc37c70e7807ede5a0
brush-cdd <mart123@vip.qq.com>
2019-03-18 16:59:39 +0800

    后台接收视频信息 图片上传的upload文件夹修改

--- application/admins/controller/Video.php
+++ application/admins/controller/Video.php
@@ -26,7 +26,38 @@ class Video extends BaseAdmin {
     //添加视频
     public function add(){
         $data['channel'] = $this->db->table('video_label')->where(array('flag'=>'channel'))->lists();
+
+        $id = (int)input('get.id');
+        $data['item'] = $this->db->table('video')->where(array('id'=>$id))->item();
+        $this->assign('data',$data);
         return $this->fetch();
+    }
+
+    //保存视频
+    public function save(){
+        $id = (int)input('post.id');
+        $data['title'] = trim(input('post.title'));
+        $data['channel_id'] = (int)input('post.channel_id');
+        $data['img'] = trim(input('post.img'));
+        $data['url'] = trim(input('post.url'));
+        $data['keyword'] = trim(input('post.keyword'));
+        $data['desc'] = trim(input('post.desc'));
+        $data['status'] = (int)input('post.status');
+
+        if ($data['title'] == ''){
+            exit(json_encode(array('code'=>1,"msg"=>'视频名称不能为空')));
+        }
+        if ($data['url'] == ''){
+            exit(json_encode(array('code'=>1,"msg"=>'视频地址不能为空')));
+        }
+
+        if ($id){
+            $this->db->table('video')->where(array('id'=>$id))->update($data);
+        }else{
+            $data['add_time'] = time();
+            $this->db->table('video')->insert($data);
+        }
+        exit(json_encode(array('code'=>0,'msg'=>'保存成功')));
     }
 
     //图片上传
@@ -35,7 +66,7 @@ class Video extends BaseAdmin {
         if ($file==null){
             exit(json_encode(array('code'=>1,'msg'=>'没有文件上传')));
         }
-        $info = $file->move(ROOT_PATH.'public'.DS.'uploads');
+        $info = $file->move(ROOT_PATH.'public'.DS.'upload');
         $ext = ($info->getExtension());
         if (!in_array($ext,array('jpg','jpeg','gif','png'))){
             exit(json_encode(array('code'=>1,'msg'=>'文件格式不支持')));
